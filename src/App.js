1240b378b06e7c5a352a1bd7f4f5b8eb139cc586
willshuffy <saputrawilly26@gmail.com>
2022-04-27 10:18:19 +0700

    Project Started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
