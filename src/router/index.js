31af6c5bc80b148831c80fb8752c5bd552914c0a
willshuffy <saputrawilly26@gmail.com>
2022-04-27 11:44:05 +0700

    Create Bottom Navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/Home';
 import Splash from '../pages/Splash';
+import Pesanan from '../pages/Pesanan';
+import Akun from '../pages/Akun';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Pesanan" component={Pesanan} />
+    <Tab.Screen name="Akun" component={Akun} />
+  </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName='MainApp'>
       <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}}/>
-      <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+      <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}}/>
     </Stack.Navigator>
   )
 }
