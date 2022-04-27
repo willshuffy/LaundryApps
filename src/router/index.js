27f5a187a60a9f94ee9d57316954a4207fa1a4e1
willshuffy <saputrawilly26@gmail.com>
2022-04-27 11:22:31 +0700

    adding Splash page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../pages/Home';
+import Splash from '../pages/Splash';
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}}/>
       <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
     </Stack.Navigator>
   )
